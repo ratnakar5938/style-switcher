0bba78be49860118177c23c75554ae9163e940ac
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-07-12 21:20:54 +0530

    text-styler update

--- scripts.js
+++ scripts.js
@@ -20,3 +20,22 @@ function changeColor() {
 if (localStorage.getItem("color") !== null) {
     changeColor();
 }
+
+const alternateTextStyles = document.querySelectorAll(".alternate-text-style");
+
+function setTextStyle(style) {
+    localStorage.setItem("style", style);
+    changeTextStyle();
+
+}
+
+function changeTextStyle() {
+    alternateTextStyles.forEach((style) => {
+        if (localStorage.getItem("style") === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }
+        else {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
